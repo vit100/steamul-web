b32919e095ad805b4759b5150eeeb324e618c08b
Vitaliy <9357021+vit100@users.noreply.github.com>
2020-04-30 19:22:36 -0400

    schedule

--- js/announcements.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  $.get('https://steamul-scheduler.azurewebsites.net/api/announcement', function(d){
-    if(d.text){
-      $('#messageBody').html(d.text)
-      $('#popup_down').show(1500);
-    }
-    })
-})
